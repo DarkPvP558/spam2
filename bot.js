12b92d48e21cc74504da103798358f656e40a167
DarkPvP558 <46353233+DarkPvP558@users.noreply.github.com>
2019-02-02 13:17:14 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,9 +4,9 @@ console.log("Scrpit By Dream");
 
 
 client.on("ready", () => {
-let channel =     client.channels.get("هنا اي دي الروم")
+let channel =     client.channels.get("538637523828604940")
 setInterval(function() {
-channel.send(`هنا كلام ال سبام`);
+channel.send(`Lol2`);
 }, 30)
 })
 
